ee7fb5cf3ad79dd5c455f90d76f80c4a62b69eb9
Vishal Kumar <ui21cs69@iiitsurat.ac.in>
2024-11-23 21:02:41 +0530

    Initial commit for Portfolio project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Vishal-6969/My_Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
